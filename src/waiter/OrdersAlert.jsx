bf3e58148802f1c9e5293d563c41cb8d8b917c7e
Nataly Escobar <natalyescobar85@gmail.com>
2022-06-07 14:06:59 -0500

    creation of the OrdesAlert component so that the waiter can view the orders that are yet to be delivered

---
+++ src/waiter/OrdersAlert.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function OrdersAlert(){
+
+
+    return(
+        <div className="material-symbols-outlined">circle_notifications</div>
+        
+    );
+}
