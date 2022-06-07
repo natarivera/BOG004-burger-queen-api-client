ab8da7412b94fe99ec4ffd76eea4e5348b544ad0
Nataly Escobar <natalyescobar85@gmail.com>
2022-06-07 18:20:25 -0500

    funcionality delete and add for summary products

--- src/waiter/Waiter.jsx
+++ src/waiter/Waiter.jsx
@@ -12,26 +12,27 @@ import OrdersAlert from "./OrdersAlert";
 
 export default class Waiter extends React.Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state = {numTable:undefined, products:new Map(), customer:''};
-        this.handleClick = this.handleClick.bind(this) ;
+        this.state = { numTable: undefined, products: new Map(), customer: '' };
+        this.handleClick = this.handleClick.bind(this);
         this.addProduct = this.addProduct.bind(this);
-        this.setCustomer= this.setCustomer.bind(this);
+        this.subProduct = this.subProduct.bind(this);
+        this.setCustomer = this.setCustomer.bind(this);
     }
 
-    handleClick(event) {                
+    handleClick(event) {
         const { id } = event.target.dataset;
-        if( id !== undefined){
-            this.setState({numTable:id});
-        }                            
-      }
-   
-      addProduct(product){
-        if(this.state.products.has(product.id)){            
+        if (id !== undefined) {
+            this.setState({ numTable: id });
+        }
+    }
+
+    addProduct(product) {
+        if (this.state.products.has(product.id)) {
             //1.1 Si esta entonces le suma 1 a la cantidad
             const item = this.state.products.get(product.id);
-            item.qty += 1;            
+            item.qty += 1;
         } else {
             //1.2 Si no, lo agrega con cantidad 1
             const item = {
@@ -39,46 +40,60 @@ export default class Waiter extends React.Component {
                 product: product
             };
             //lo agregamos al map
-            this.state.products.set(product.id, item);            
+            this.state.products.set(product.id, item);
         }
-        this.setState({products:this.state.products});
-      }
-
-      setCustomer(customer){
-        this.setState({customer});
-
-      }
-
-    render(){
-        const  tablesCount =  this.props.tablesCount;
-        let arrayTables = [];
-        for (var i=0 ; i<tablesCount; i++) {
-            arrayTables.push(
-                <div key={i} data-id={i+1} className="item-Table">
-                    <div data-id={i+1} className="material-symbols-outlined icon-Table">table_restaurant</div>
-                    <div data-id={i+1}>Mesa {i+1}</div>
-                </div>                
-            )
+        this.setState({ products: this.state.products });
     }
+
+    subProduct(product) {
+        //1.traer el objeto del map
+        const item = this.state.products.get(product.id);
+        //1.1 Si tiene una cantidad mayor que uno 
+        if (item.qty > 1) {
+            //1.1.2 restar uno
+            item.qty -= 1;
+        } else { //1.2 Si no, es = 1               
+            //1.2.1 quitarlo del map
+            this.state.products.delete(product.id);
+        }
+        this.setState({ products: this.state.products });
+    }
+
+    setCustomer(customer) {
+        this.setState({ customer });
+
+    }
+
+    render() {
+        const tablesCount = this.props.tablesCount;
+        let arrayTables = [];
+        for (var i = 0; i < tablesCount; i++) {
+            arrayTables.push(
+                <div key={i} data-id={i + 1} className="item-Table">
+                    <div data-id={i + 1} className="material-symbols-outlined icon-Table">table_restaurant</div>
+                    <div data-id={i + 1}>Mesa {i + 1}</div>
+                </div>
+            )
+        }
         return <div className="Waiter">
             {!this.state.numTable && <Logo />}
             {!this.state.numTable && <div className="table" onClick={this.handleClick.bind(this)}>{arrayTables}</div>}
-            
-            { this.state.numTable && 
-                <div className="Order">    
-                    <Logo />                
-                    <div className="WaiterHeader">   
-                        <OrdersAlert/>
-                        <NameWaiter user={this.props.user}/>                        
-                    </div>   
-                    <Customer setCustomer={this.setCustomer} customer={this.state.customer}/>
-                    <div/>        
-                    <Summary products={this.state.products} addProduct={this.addProduct} user={this.props.user}/>
-                    <Products numTable={this.state.numTable} addProduct={this.addProduct} user={this.props.user}/>
+
+            {this.state.numTable &&
+                <div className="Order">
+                    <Logo />
+                    <div className="WaiterHeader">
+                        <OrdersAlert />
+                        <NameWaiter user={this.props.user} />
+                    </div>
+                    <Customer setCustomer={this.setCustomer} customer={this.state.customer} />
+                    <div />
+                    <Summary products={this.state.products} addProduct={this.addProduct} user={this.props.user} subProduct={this.subProduct} />
+                    <Products numTable={this.state.numTable} addProduct={this.addProduct} user={this.props.user} />
                 </div>
-            }            
+            }
             {!this.props.user && (<Navigate to="/" />)}
-    
+
         </div>
     }
 }
